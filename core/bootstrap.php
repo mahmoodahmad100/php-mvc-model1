e29785901b6e5dde39d7975e91478e8c4a0c90ff
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-06 19:35:01 +0200

    init autoload classes

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,3 +2,16 @@
 
 require_once ROOT . DS . 'config' . DS . 'config.php';
 require_once ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php';
+
+/**
+* autoload classes
+*/
+
+spl_autoload_register(function($class){
+	if(file_exists())
+		// include the file
+	elseif(file_exists())
+		// include the file
+	elseif(file_exists())
+		// include the file
+});
