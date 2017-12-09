6006afe84e6b56baa0ead5d0d9367cecaef2ec6f
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-09 21:06:06 +0200

    controller route

--- core/Router.php
+++ core/Router.php
@@ -3,6 +3,11 @@
 class Router
 {
 	public static function route($url){
+		
+		// controller
+		$controller = (isset($url[0]) && $url[0] != '') ? ucwords($url[0]) : DEFAULT_CONTROLLER ;
+		$controller_name = $controller;
+		array_shift($url);
 		dieDump($url);
 	}
 }
