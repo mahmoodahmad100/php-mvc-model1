0a9967bd67f451eb6067e8b9d4c1cf69f179baec
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-12 23:42:39 +0200

    action route

--- core/Router.php
+++ core/Router.php
@@ -8,8 +8,13 @@ class Router
 		$controller = (isset($url[0]) && $url[0] != '') ? ucwords($url[0]) : DEFAULT_CONTROLLER ;
 		$controller_name = $controller;
 		array_shift($url);
-		dieDump($url);
 
 		// action
+		$action = (isset($url[0]) && $url[0] != '') ? $url[0]."Action" : "indexAction" ;
+		$action_name = $action;
+		array_shift($url);
+		
+		echo $controller . "<br>";
+		echo $action . "<br>";
 	}
 }
