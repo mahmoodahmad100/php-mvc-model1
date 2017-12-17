ac92eae05b14d88b473a997e18c18bbce4120c15
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-17 21:22:20 +0200

    _set_reporting()

--- core/Application.php
+++ core/Application.php
@@ -10,7 +10,18 @@ class Application
 
 	private function _set_reporting()
 	{
-
+		if(DEBUG)
+		{
+			error_reporting(E_ALL);
+			ini_set('display_errors', 1);
+		}
+		else
+		{
+			error_reporting(0);
+			ini_set('display_errors', 0);
+			ini_set('log_errors', 1);
+			ini_set('errors_log', ROOT . DS . 'tmp' . DS . 'logs' . DS . 'errors.log');
+		}
 	}
 
 	private function _unregister_globals()
