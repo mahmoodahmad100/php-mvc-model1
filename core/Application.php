726fdb37394531f3d105aedabafb42d32d4054b1
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-17 21:07:38 +0200

    ninit 2 methods

--- core/Application.php
+++ core/Application.php
@@ -4,7 +4,18 @@ class Application
 {
 	public function __construct()
 	{
-		
+		$this->_set_reporting();
+		$this->_unregister_globals();
+	}
+
+	private function _set_reporting()
+	{
+
+	}
+
+	private function _unregister_globals()
+	{
+
 	}
 }
 
