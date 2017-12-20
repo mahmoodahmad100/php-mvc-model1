5d3dbe7b773fea43d7cf8111f161760e7f999608
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-20 23:54:58 +0200

    comments

--- config/config.php
+++ config/config.php
@@ -1,10 +1,13 @@
 <?php 
 
+// system
 define('PROOT', '/mvc//');
 define('DEBUG', true);
 
+// core
 define('DEFAULT_CONTROLLER', 'Home');
 define('DEFAULT_ACTION', 'indexAction');
 
+// layout
 define('DEFAULT_LAYOUT', 'default');
 define('SITE_TITLE', "MVC PROJECT");
