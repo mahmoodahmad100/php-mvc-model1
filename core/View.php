eda8249bc24707570921759fe4d562960e76ce55
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-21 22:23:28 +0200

    die() if the view doesn't exist

--- core/View.php
+++ core/View.php
@@ -19,5 +19,7 @@ class View
 			include ROOT . DS . 'app' . DS . 'views' . DS . $viewString . '.php';
 			include ROOT . DS . 'app' . DS . 'views' . DS . 'layouts' . DS . $this->_layout . '.php';
 		}
+		else
+			die('the view ' . $viewName. ' does not found');
 	}
 }
