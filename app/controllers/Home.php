422a9b7942304fdc2d1ef2cb4e7f1070d0e8183f
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-26 07:31:29 +0200

    init the constructor & indexAction

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,5 +2,13 @@
 
 class Home extends Controller
 {
-	
+	public function __construct($contorller, $action)
+	{
+		parent::__construct($contorller, $action);
+	}
+
+	public function indexAction()
+	{
+		die("this is the home/indexAction()");
+	}
 }
