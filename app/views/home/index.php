9daa32a4237145c84a6587090702c21fff32011e
mahmoodahmad100 <mahmood073@gmail.com>
2018-01-17 21:07:33 +0200

    testing start ===> head & body

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,11 +1,10 @@
 <?php $this->setSiteTitle('home page'); ?>
 
 <?php $this->start('head'); ?>
-
+<meta name="author" content="test">
 <?php $this->end(); ?>
 
 <?php $this->start('body'); ?>
-
-<?php $this->end(); ?>
-
 <h3>this is the home/index view</h3>
+<button class="btn btn-primary">hello world</button>
+<?php $this->end(); ?>
